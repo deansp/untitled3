addcc28d0b9c01fa898101f3b10a5c102a9a3e36
deanspecht <deansp@web.de>
2024-04-16 11:49:13 +0200

    add challenges.ts

--- src/challenges.ts
+++ src/challenges.ts
@@ -6,41 +6,43 @@ type Student={
     age: number
     grades: Grades[]
 }
-
 let student1:Student = {
     firstName: "Dean",
     lastName: "Specht",
     age: 36,
     grades: ["*","1","3","2","A"]
 }
-
 let student2:Student = {
-    firstName: "Inge",
+    firstName: "IngeMitLangemNamen",
     lastName: "Karl",
     age: 12,
     grades: ["6","6","F","*","*"]
 }
-
 let student3:Student = {
     firstName: "bla",
-    lastName: "nerv",
+    lastName: "ne",
     age: 91,
     grades: ["1","2","A","*","2"]
 }
 
-function printStudents(printStudent:Student,){
-    let underline:string[]=[];
-    for(let i:number=0;i<30;i++){
-        underline.push("=")
-    }
+function printStudents(printStudent:Student){
     console.log(
     printStudent.firstName +
     printStudent.lastName +
     "("+ printStudent.age +")"+
-    "\n"+underline.join('')+
+    "\n"+calcUnderline(printStudent).join('')+
     "\nGrades: " + printStudent.grades +
         "\n"
     )}
+
+function calcUnderline(printStudent:Student):String[]{
+    let underline:string[]=[];
+    const varLength:number=printStudent.firstName.length+printStudent.lastName.length+4
+    for(let i:number=0;i<varLength;i++){
+        underline.push("=")
+    }
+    return underline
+}
 
 printStudents(student1)
 printStudents(student2)
